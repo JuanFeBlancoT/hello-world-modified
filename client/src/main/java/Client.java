ceaf24c36a89ade9df9851871c1c4ce138361021
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2023-02-23 19:14:44 -0500

    Sever responding with clients hostname

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,22 +1,32 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Client
 {
     public static void main(String[] args)
     {
         java.util.List<String> extraArgs = new java.util.ArrayList<>();
 
-        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args,"config.client",extraArgs))
-        {
+        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args,"config.client",extraArgs)) {
             //com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
             Demo.PrinterPrx twoway = Demo.PrinterPrx.checkedCast(
-                communicator.propertyToProxy("Printer.Proxy")).ice_twoway().ice_secure(false);
+                    communicator.propertyToProxy("Printer.Proxy")).ice_twoway().ice_secure(false);
             //Demo.PrinterPrx printer = Demo.PrinterPrx.checkedCast(base);
-            Demo.PrinterPrx printer = twoway.ice_oneway();
+            Demo.PrinterPrx printer = twoway.ice_twoway();
 
-            if(printer == null)
-            {
+            if (printer == null) {
                 throw new Error("Invalid proxy");
             }
-            printer.printString("Hello World!");
+
+            String hostname;
+            try {
+                //get hostname
+                hostname = InetAddress.getLocalHost().getHostName();
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            }
+            //send hostname
+            printer.printString(hostname + " says " + "Hello World!");
         }
     }
 }
