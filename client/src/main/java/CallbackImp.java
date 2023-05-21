dc910f1631d74201e3c3797ba0661a2687c0f6bf
JuanFeBlancoT <72984915+JuanFeBlancoT@users.noreply.github.com>
2023-05-20 21:56:04 -0500

    Tried to add functionality to server

--- client/src/main/java/CallbackImp.java
+++ client/src/main/java/CallbackImp.java
@@ -8,8 +8,18 @@ public class CallbackImp implements Demo.Callback {
 
     private ChatManagerPrx server;
 
+    private int request_size = 10000;
+    private int timeout_time_bot = 0;
+    private int timeout_time_top = 100;
+
     CallbackImp(ChatManagerPrx m) {
         server = m;
+    }
+
+
+  @Override
+    public void receiveMessage(String msg, Current current) {
+        System.out.println(msg);
     }
 
     @Override
@@ -18,6 +28,29 @@ public class CallbackImp implements Demo.Callback {
         String state[] = server.getState();
 
         System.out.println("Callback exec: " + Arrays.toString(state));
+    }
+
+    public void testFiboTimeout() {
+        int index = 0;
+        String hostname = "";
+        ArrayList<CompletableFuture<String>> calculatedNumbers = new ArrayList<>();
+
+        try {
+            int[] array = createFiboTestArray();
+            while(index < array.length - 1){
+                int number = array[index];
+                senderPrx.printFibonacci(hostname, String.valueOf(number));
+                index++;
+            }
+
+        } catch (TimeoutException timeoutException) {
+            System.out.println("TIMEOUT");
+            System.out.println(index+1);
+        }
+    }
+
+    private int[] createFiboTestArray(){
+        return new Random().ints(request_size, timeout_time_bot, timeout_time_top + 1).toArray();
     }
 
 }
--- server/src/main/java/ChatManagerImp.java
+++ server/src/main/java/ChatManagerImp.java
@@ -8,18 +8,26 @@ import Demo.CallbackPrx;
 
 public class ChatManagerImp implements Demo.ChatManager {
 
+
+    //Store messages sent to the server
     private List<String> messages;
     // Semaphore mSemaphore = new Semaphore(1);
     private CallbackPrx callbackPrx;
 
+    //Sets the new lsit of messages of the server
     ChatManagerImp() {
         messages = new ArrayList<>();
     }
 
     @Override
-    public void subscribe(CallbackPrx callback, Current current) {
+    public void subscribe(CallbackPrx callback, String hostname, Current current) {
         System.out.println("subscribe");
         this.callbackPrx = callback;
+    }
+
+    @Override
+    public String registerClient(HelloWorldCallbackReceiverPrx proxy, String hostname, Current current) {
+        return null;
     }
 
     @Override
@@ -31,6 +39,27 @@ public class ChatManagerImp implements Demo.ChatManager {
             state[i] = messages.get(i);
         }
         return state;
+    }
+
+  @Override
+    public String printFibonacci(String hostname, String input, Current current) {
+        String message = hostname + ":" + input;
+        System.out.println(message);
+
+        int output = 0;
+        if(validateInputIsNumber(input)){
+            int numberSent = Integer.parseInt(input);
+            result = fibonacciMethod(inputNumber);
+            //showFibonacciSequence(hostname, inputNumber);
+            return output + "";
+        }else{
+            return input;
+        }
+        
+    }
+    
+    private boolean validateInputIsNumber(String input){
+        return input.matches("\\d+");
     }
 
     @Override
@@ -48,5 +77,42 @@ public class ChatManagerImp implements Demo.ChatManager {
             callbackPrx.notifyCallback();
         }).start();
     }
+
+    @Override
+    public void receiveMessage(String msg, Current current) {
+        System.out.println(msg);
+    }
+
+
+public int fibo(int number) {
+    public String fibonacciMethod(String information, com.zeroc.Ice.Current current)
+    {
+        //VARIABLES
+        int num1 = 1, num2 = 1, sum=0;
+        
+        String[] clientInformation = information.split(":");
+
+        String clientInfo = clientInformation[0]; // client info
+        //parce
+        int fibonacciNumber = Integer.valueOf(clientInformation[1]); // fibonnaci number
+
+        int[] fibonacciNumbers = new int[fibonacciNumber];
+
+        //FIBONACCI
+        if (fibonacciNumber==1 || fibonacciNumber==2){
+            return "Client: " +clientInfo+ " the last number of the Fibonnaci Succession is: " +fibonacciNumber;
+        }else{
+            for (int i=2;i<fibonacciNumber;i++) {
+                sum = num1 + num2;
+                fibonacciNumbers[i] = sum;
+                num1 = num2;
+                num2 = sum;
+                System.out.println(sum);
+            }
+            return "Client: " +clientInfo+ " the last number of the Fibonnaci Succession is: " +fibonacciNumbers[fibonacciNumber-1];
+        }
+    }
+
+  
 
 }
